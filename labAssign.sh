f34a5d64196f6e02d6ea8d17f78edfd6c6d6fe96
Erika <erikaeburdon@gmail.com>
2022-02-21 16:19:08 -0800

    Structure is good, still need to parse

--- labAssign.sh
+++ labAssign.sh
@@ -1,28 +1,37 @@
 
 #!/bin/bash
 
-runDataGather()
+printError()
 {
-if whois $1 | grep Contact; then
-    echo OK
-else
-    echo FAIL
-fi
-
+    echo $'\nThat website does not seem to exist.'
+    echo "Please use a real website in the format of:"
+    echo "www.website.com or website.com"
 }
 
 
 
-echo Hello World!
+runDataGather()
+{
+    OUTPUT=$(whois $1)
+    #check to make sure that the whois command worked.
+    if [ "$?" != "0" ]; then
+        #command failed, kick out and print error
+        printError
+        return 1
+    fi
+    # echo "$OUTPUT"
+    # echo "$OUTPUT" | grep Country
+    
+    echo "$OUTPUT" 
+    return 0
+}
 
-#LOCAL_TEXT ="Hello"
 
+#program start here
 echo "What website would you like to gather data from?"
 read WEBSTITE_NAME
-echo "Grabbing information on: $WEBSTITE_NAME"
+echo "Grabbing information on the website: $WEBSTITE_NAME"
 
+runDataGather $WEBSTITE_NAME
 
-
-
-
-
+echo "This script was built and tested on whois-server version 1.102.2 (HEREFORD)"
